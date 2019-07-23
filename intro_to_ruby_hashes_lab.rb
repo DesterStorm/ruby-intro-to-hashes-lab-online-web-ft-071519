4490263b9c0dbb0656f0cd54c02691ae0826744f
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-07-23 19:20:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,3 +1,19 @@
+def monopoly
+	monopoly = {:railroads => {}}
+end
+
+def monopoly_with_second_tier
+monopoly = {
+  :railroads => {
+    :pieces => 4, 
+    :names => {},
+    :rent_in_dollars => {}
+  }
+}
+monopoly = {:railroads => {:pieces => 4}, :names => {} }
+end
+
+
 def monopoly_with_third_tier
 	monopoly = {
   :railroads => {
